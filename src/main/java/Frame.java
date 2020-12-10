b5f2d8602310a2c284e7b4f370c59578bf6e220d
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-11 02:54:48 +0800

    empty message

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author huisheng.jin
  * @date 2020/12/11.
  */
 public class Frame {
+    private List<Integer> rollPins = new ArrayList<>();
     public Integer getRollPins() {
-        return 0;
+        return rollPins.stream().reduce(0,(x,y)->x+y);
     }
 
     public void add(int pins) {
-
+        rollPins.add(pins);
     }
 
     public boolean isComplete() {
