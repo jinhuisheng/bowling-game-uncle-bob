7db59c3ff0626b86549fa2ebb03908588ccfce19
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-11 02:57:08 +0800

    empty message

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -10,9 +10,21 @@ public class GameTest {
     @Test
     void should_score_0_when_all_rolls_are_zero() {
         Game game = new Game();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
+            game.roll(0);
             game.roll(0);
         }
         assertThat(game.score()).isEqualTo(0);
     }
+
+//    @Test
+//    void should_score_90_when_all_rolls_are_miss() {
+//        Game game = new Game();
+//        for (int i = 0; i < 10; i++) {
+//            game.roll(5);
+//            game.roll(4);
+//        }
+//        assertThat(game.score()).isEqualTo(0);
+//    }
+
 }
