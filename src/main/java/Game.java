9fde8be30552f59cc74b25637840a154b3be3608
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-11 04:09:24 +0800

    empty message

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,12 +6,13 @@ import java.util.List;
  * @date 2020/12/11.
  */
 public class Game {
-
     private static final int FRAME_COUNT = 10;
     private final List<Frame> frames;
     private Integer index;
+    private FramesContainer framesContainer;
 
     public Game() {
+        framesContainer = new FramesContainer();
         frames = initFrames();
         index = 0;
     }
