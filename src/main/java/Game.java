aca00610ea412f87fbe162f51322f27e2f3d8243
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-11 03:05:42 +0800

    empty message

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -46,6 +46,14 @@ public class Game {
         if (frames.get(0).getRollPins() == 10) {
             return 150;
         }
+        int score = 0;
+        for (int index = 0; index < 10; index++) {
+            score += countFrameScore(index);
+        }
         return frames.stream().map(Frame::getRollPins).reduce(0, Integer::sum);
+    }
+
+    private int countFrameScore(int index) {
+        return 0;
     }
 }
